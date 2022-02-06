035d09506e1cf8cc4c9ba92b73adef4b1cb90e78
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-02-06 12:35:51 -0600

    added inquirer prompts and classes for employees, manager, engineer, adn intern

---
+++ index.js
@@ -0,0 +1,3 @@
+var addEmployees = require('./lib/Prompts')
+
+addEmployees()
---
+++ src/pageTemplate.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+const Manager = require('../lib/Manager')
+const Engineer = require('../lib/Engineer')
+const Intern = require('../lib/Intern')
+
+
+const generateEmployeeCardHtml = () => {
+
+}
+
+const generatePageHtml = () => {
+
+}
+
+fs.writeFile()
